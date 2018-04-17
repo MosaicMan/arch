300031f75b1f8bd1341a42ceb81df4d69e094c08
MosaicMan <34198823+MosaicMan@users.noreply.github.com>
2018-04-17 14:01:20 -0400

    Update install.sh

--- .gitignore/install.sh
+++ .gitignore/install.sh
@@ -2,12 +2,12 @@
 echo "*** Resize archiso ***"
 mount -o remount,size=2G /run/archiso/cowspace
 
-echo "*** Update Pacman database ***"
-pacman -Syy
-
 echo "*** Configure Reflector ***"
 pacman -S reflector
 reflector -c "United States" -f 12 -l 10 -n 12 --save /etc/pacman.d/mirrorlist
+
+echo "*** Update Pacman database ***"
+pacman -Syy
 
 echo "*** Install base system ***"
 pacstrap -i /mnt base base-devel btrfs-progs
