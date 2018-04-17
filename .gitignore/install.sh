64191bf515e03b532ca417d94f103409e5f33339
MosaicMan <34198823+MosaicMan@users.noreply.github.com>
2018-04-17 15:38:42 -0400

    Update install.sh

--- .gitignore/install.sh
+++ .gitignore/install.sh
@@ -5,19 +5,19 @@ echo "*** Resize archiso ***"
 mount -o remount,size=2G /run/archiso/cowspace
 
 echo "*** Create partitions ***"
-parted -s -a optimal /dev/sda mklabel gpt -- mkpart ESP fat32 1 1024 set 1 esp on set 1 boot on mkpart primary btrfs 1024 -0
+parted -s -a -f optimal /dev/sda mklabel gpt -- mkpart ESP fat32 1 1024 set 1 esp on set 1 boot on mkpart primary btrfs 1024 -0
 mkfs.fat -F32 /dev/sda1
 mkfs.btrfs /dev/sda2
 mount /dev/sda2 /mnt
 mkdir -p /mnt/boot
 mount /dev/sda1 /mnt/boot
 
+echo "*** Update Pacman database ***"
+pacman -Syy
+
 echo "*** Configure Reflector ***"
 pacman -S --noconfirm reflector
 reflector -c "United States" -f 12 -l 10 -n 12 --save /etc/pacman.d/mirrorlist
-
-echo "*** Update Pacman database ***"
-pacman -Syy
 
 echo "*** Install base system ***"
 pacstrap /mnt base base-devel btrfs-progs
