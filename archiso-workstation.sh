410c3ce898a35ddf3341d2afa4b1853486c49d9c
Juan Carlos <juancq@development.localdomain>
2018-04-19 01:17:46 -0400

    Update setup scripts

--- archiso-workstation.sh
+++ archiso-workstation.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
+#echo "*** Resize ramdisk ***"
+#mount -o remount,size=2G /run/archiso/cowspace
 
-echo "*** Resize ramdisk ***"
-mount -o remount,size=2G /run/archiso/cowspace
+echo "*** Update Pacman database ***"
+pacman -Syy
+pacman -S git btrfs-progs
+
+sleep 3
 
 echo "*** Create & format partitions ***"
 umount -R /mnt || /bin/true
@@ -10,29 +15,38 @@ parted -s -a optimal /dev/sda mklabel gpt -- mkpart ESP fat32 1 1024 set 1 esp o
 mkfs.fat -f -F32 /dev/sda1
 mkfs.btrfs -f -L Arch /dev/sda2
 
+sleep 3
+
 echo "*** Create btrfs subvolumes ***"
 mount /dev/sda2 /mnt
-btrfs subvol create /mnt/@OS
+btrfnanos subvol create /mnt/@OS
 umount -R /mnt
 mount -o compress=lzo,noatime,subvol=@OS /dev/sda2 /mnt
 btrfs subvol create /mnt/home
+
+sleep 3
 
 echo "*** Mount boot partition ***"
 mkdir -p /mnt/boot
 mount /dev/sda1 /mnt/boot
 
-echo "*** Update Pacman database ***"
-pacman -Syy
+sleep 3
 
 echo "*** Configure Reflector ***"
 pacman -S --noconfirm reflector
 reflector -c "United States" -f 12 -l 10 -n 12 --save /etc/pacman.d/mirrorlist
 
+sleep 3
+
 echo "*** Install base system ***"
 pacstrap /mnt base base-devel btrfs-progs refind-efi efibootmgr git mercurial
 
+sleep 3
+
 echo "*** Generate fstab file ***"
 genfstab -U -p /mnt >> /mnt/etc/fstab
+
+sleep 3
 
 echo "*** Run base install script ***"
 cp base-install.sh /mnt/.
@@ -42,4 +56,7 @@ arch-chroot /mnt ./base-install.sh
 cp refind.conf /mnt/boot/EFI/refind/refind.conf
 cp arch.refind.conf /mnt/boot/EFI/refind/arch.refind.conf
 rm /mnt/base-install.sh
+
+sleep 3
+
 umount -R /mnt
