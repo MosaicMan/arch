5d122366ec78efa74f4d09c5e9e41abf92a58449
MosaicMan <>
2018-04-20 03:22:33 -0400

    Update setup scripts

--- keyring/keyring-install.sh
+++ keyring/keyring-install.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
-pacman -S gnome-keyring libsecret seahorse
+sudo pacman -S gnome-keyring libsecret seahorse
 
-echo "eval $(/usr/bin/gnome-keyring-daemon --start --components=pkcs11,secrets,ssh)" >> /home/juancq/.xinitrc
+# Add and uncomment these lines to .bashrc and/or .zshrc
+# if [ -n "$DESKTOP_SESSION" ];then
+#     eval $(gnome-keyring-daemon --start)
+#     export SSH_AUTH_SOCK
+# fi
 
-secret-tool store --label="KeePass" keepass passdb.kdbx
+# Logout and login to let the diplay manager create the default keyring
+
+# Creat a unique key-value keyring entry labeled "KeePass"
+# secret-tool store --label="KeePass" keepass passdb.kdbx
 
